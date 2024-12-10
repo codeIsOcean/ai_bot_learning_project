eb04195c863bbff3c5155f8257078f8e8f643b8a
QanaSar <qqanatt@gmail.com>
2024-12-11 00:52:47 +0500

    Initial Commit

---
+++ app/user.py
@@ -0,0 +1,10 @@
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import CommandStart
+
+user = Router()
+
+
+@user.message(CommandStart)
+async def smd_start(message: Message):
+    await message.answer('Добро пожаловать')
---
+++ run.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from config import TOKEN
+from app.admin import admin
+from app.user import user
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.include_routers(user, admin)
+    await dp.start_polling(Bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
